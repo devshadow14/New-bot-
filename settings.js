f2bf4bbf658cab7bf4540f20fae5c64ed6f61c28
devshadow14 <nd172237@gmail.com>
2026-09-07 21:39:02 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // 🤖 BASIC INFO
+  botName: "DEV MICHAEL SCOFIELD-MD 👑",
+  ownerName: "Dev Michael Scofield",
+  ownerNumber: "221758535949",
+  sudoNumbers: ["221776227173"],
+  prefix: ".",
+  version: "2.0.0",
+  
+  // ⚙️ SYSTEM & ENVIRONMENT
+  sessionName: "riftmd-session",
+  timezone: "America",
+  language: "en",
+
+  // 🎨 STICKER UI CUSTOMIZATION
+  packname: "Created by",
+  author: "DEV MICHAEL SCOFIELD-MD 💜",
+
+  // 🛡️ SECURITY & CONTROL OPTIONS
+  antiLink: false,
+  antiSpam: true,
+  autoReadMessages: false,
+  autoReact: true,
+
+  // 🔌 ADVANCED FEATURES & API CONNECTIONS
+  geminiApiKey: "",
+  mongodbUrl: "",
+
+  // 💬 AUTOMATIC MESSAGES
+  welcomeMessage: true,
+  goodbyeMessage: true
+};
