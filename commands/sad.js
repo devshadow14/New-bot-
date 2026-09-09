ab29ef11941dd1f7cd50fdcc690ad63bbab371c6
devshadow14 <nd172237@gmail.com>
2026-09-09 21:21:54 +0000

    Create sad.js

---
+++ commands/sad.js
@@ -0,0 +1,11 @@
+const MESSAGES = [
+    "😢 Journée un peu difficile aujourd'hui...",
+    "😔 Pas la forme, besoin d'un câlin virtuel.",
+    "🥺 Petit coup de blues passager."
+];
+
+module.exports = async (sock, m, args) => {
+    const from = m.key.remoteJid;
+    const text = MESSAGES[Math.floor(Math.random() * MESSAGES.length)];
+    await sock.sendMessage(from, { text }, { quoted: m });
+};
