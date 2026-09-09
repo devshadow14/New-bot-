8b9ac071d2951c7edfbd30f4e7430fa4c49f8e26
devshadow14 <nd172237@gmail.com>
2026-09-09 21:23:40 +0000

    Create confused.js

---
+++ commands/confused.js
@@ -0,0 +1,11 @@
+const MESSAGES = [
+    "🤔 Attends... je ne comprends plus rien là.",
+    "😵‍💫 Ma tête tourne, trop d'informations d'un coup.",
+    "❓ Quoi ? Répète, j'ai zappé."
+];
+
+module.exports = async (sock, m, args) => {
+    const from = m.key.remoteJid;
+    const text = MESSAGES[Math.floor(Math.random() * MESSAGES.length)];
+    await sock.sendMessage(from, { text }, { quoted: m });
+};
