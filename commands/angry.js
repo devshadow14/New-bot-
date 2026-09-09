03a793bb420a9e1d97324c6a086878ca6c22ca9b
devshadow14 <nd172237@gmail.com>
2026-09-09 21:19:59 +0000

    Create angry.js

---
+++ commands/angry.js
@@ -0,0 +1,11 @@
+const MESSAGES = [
+    "😠 Grrr... quelqu'un a réveillé le côté sombre !",
+    "😡 STOP ! Tu commences à m'énerver sérieusement.",
+    "🔥 Je bous de rage intérieurement là."
+];
+
+module.exports = async (sock, m, args) => {
+    const from = m.key.remoteJid;
+    const text = MESSAGES[Math.floor(Math.random() * MESSAGES.length)];
+    await sock.sendMessage(from, { text }, { quoted: m });
+};
