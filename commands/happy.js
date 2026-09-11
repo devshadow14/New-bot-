16aca9303c80ececee0acc2921c98e69c574013f
devshadow14 <nd172237@gmail.com>
2026-09-11 20:52:28 +0000

    Update happy.js

--- commands/happy.js
+++ commands/happy.js
@@ -1,11 +1,13 @@
 const MESSAGES = [
-    "😄 Journée parfaite, rien ne peut gâcher ma bonne humeur !",
-    "🥳 Trop content là, tout va bien dans le meilleur des mondes !",
-    "😁 Sourire jusqu'aux oreilles, la vie est belle."
+    "Journée parfaite, rien ne peut gâcher ma bonne humeur !",
+    "Trop content là, tout va bien dans le meilleur des mondes !",
+    "Sourire jusqu'aux oreilles, la vie est belle."
 ];
 
 module.exports = async (sock, m, args) => {
     const from = m.key.remoteJid;
     const text = MESSAGES[Math.floor(Math.random() * MESSAGES.length)];
-    await sock.sendMessage(from, { text }, { quoted: m });
+    await sock.sendMessage(from, {
+        text: `╭━━━〔 😄 *HAPPY* 〕━━━⬣\n┃ ${text}\n╰━━━━━━━━━━━━━━━━━━━━⬣`
+    }, { quoted: m });
 };
