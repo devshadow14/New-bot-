4da8804c0705647d8d89d10827fdf6378e22bf5c
devshadow14 <nd172237@gmail.com>
2026-09-11 20:43:42 +0000

    Create ban3.js

---
+++ commands/ban3.js
@@ -0,0 +1,14 @@
+const settings = require("../settings");
+
+module.exports = async (sock, m, args) => {
+    const from = m.key.remoteJid;
+    const ownerNumber = settings.ownerNumber.replace(/[^0-9]/g, "");
+
+    await sock.sendMessage(from, {
+        text:
+            `╭━━━〔 🚫 *BAN 3* 〕━━━⬣\n` +
+            `┃ Cette fonctionnalité est\n┃ réservée, veuillez contacter\n┃ le propriétaire du bot.\n┃\n` +
+            `┃ 📱 wa.me/${ownerNumber}\n` +
+            `╰━━━━━━━━━━━━━━━━━━━━⬣`
+    }, { quoted: m });
+};
