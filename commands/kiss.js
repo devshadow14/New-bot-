25eafbd36140c4eec2dbc1e9af74b3fc591666b5
devshadow14 <nd172237@gmail.com>
2026-09-13 01:21:21 +0000

    Create kiss.js

---
+++ commands/kiss.js
@@ -0,0 +1,29 @@
+const { fetchAnimeGif } = require("../lib/animeGif");
+
+module.exports = async (sock, m, args) => {
+    const from = m.key.remoteJid;
+    const sender = m.key.participant || m.key.remoteJid;
+    const quoted = m.message?.extendedTextMessage?.contextInfo;
+    const target = quoted?.mentionedJid?.[0] || quoted?.participant;
+
+    try {
+        const url = await fetchAnimeGif('kiss', 'kiss');
+
+        let caption = "😘 *KISS*";
+        let mentions = [];
+        if (target) {
+            caption = `😘 @${sender.split("@")[0]} embrasse @${target.split("@")[0]}`;
+            mentions = [sender, target];
+        }
+
+        await sock.sendMessage(from, {
+            video: { url },
+            gifPlayback: true,
+            caption,
+            mentions
+        }, { quoted: m });
+
+    } catch (error) {
+        await sock.sendMessage(from, { text: `❌ API indisponible pour .kiss: ${error.message}` }, { quoted: m });
+    }
+};
